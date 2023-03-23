173617343f86d657c957c52b19737ecdc685d931
avROCKET <119268304+avROCKET@users.noreply.github.com>
2023-03-23 01:38:40 -0400

    Youtube API
    To listen to music, CSS is also finished with this commit.

---
+++ src/components/YoutubePlayer.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './YouTubePlayer.module.css';
+
+const YouTubePlayer = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [videoList, setVideoList] = useState([]);
+  const [selectedVideoId, setSelectedVideoId] = useState('');
+  const [isInterfaceVisible, setIsInterfaceVisible] = useState(true);
+
+  const API_KEY = 'AIzaSyBAkrVmn0qxOdVYWqC94R3qq9EnH4fLV3Y';
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const searchVideos = async (e) => {
+    e.preventDefault();
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchTerm}&key=${API_KEY}&maxResults=6`
+    );
+
+    setVideoList(response.data.items);
+  };
+
+  const selectVideo = (videoId) => {
+    setSelectedVideoId(videoId);
+  };
+
+  const toggleInterfaceVisibility = () => {
+    setIsInterfaceVisible(!isInterfaceVisible);
+  };
+
+  return (
+    <div className={styles.container}>
+      <button onClick={toggleInterfaceVisibility} className={styles.toggleButton}>
+        {isInterfaceVisible ? 'Hide Interface' : 'Show Interface'}
+      </button>
+      
+      {isInterfaceVisible && (
+        <div className={styles.interface}>
+          <form className={styles.form} onSubmit={searchVideos}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Search YouTube videos"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+            <button className={styles.button} type="submit">Search</button>
+          </form>
+          {videoList.length > 0 && (
+            <ul className={styles.videoList}>
+              {videoList.map((video) => (
+                <li
+                  className={styles.videoItem}
+                  key={video.id.videoId}
+                  onClick={() => selectVideo(video.id.videoId)}
+                >
+                  <img
+                    className={styles.thumbnail}
+                    src={video.snippet.thumbnails.default.url}
+                    alt={video.snippet.title}
+                  />
+                  <span className={styles.title}>{video.snippet.title}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
+
+      {selectedVideoId && (
+        <div className={styles.player}>
+          <iframe
+            title="YouTube Video Player"
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${selectedVideoId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default YouTubePlayer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,9 @@ import TableNotes from "../components/table.js";
 import NoteModal from "../components/noteEditorModal.js";
 import NoteViewerModal from '../components/noteViewerModal';
 import WeatherB from "../components/WeatherB.js";
-import SpotifyPlayerComponent from "../components/Spotify.js";
-import SpotifyAuthorizationComponent from "../components/SpotifyAuth.js";
+import YouTubePlayer from "../components/YoutubePlayer.js";
+//import SpotifyPlayerComponent from "../components/Spotify.js";
+//import SpotifyAuthorizationComponent from "../components/SpotifyAuth.js";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -23,11 +24,11 @@ export default function Home() {
   const [isEdit, setIsEdit] = useState(false);
   const [openEditorModal, setOpenEditorModal] = useState(false);
   const [openViewerModal, setOpenViewerModal] = useState(false);
-  const [showSpotifyAuth, setShowSpotifyAuth] = useState(false);
+  //const [showSpotifyAuth, setShowSpotifyAuth] = useState(false);
 
-  const handleSpotifyAuthButtonClick = () => {
-    setShowSpotifyAuth(true);
-  };
+  //const handleSpotifyAuthButtonClick = () => {
+    //setShowSpotifyAuth(true);
+  //};
 
   // NOTE: If user is not signed in, they will be routed to the Login page
   useEffect(() => {
@@ -180,12 +181,7 @@ export default function Home() {
           }}
           note={note}
         />)}
-        <SpotifyPlayerComponent />
-        <div>
-          <h1>Spotify Auth Test</h1>
-          <button onClick={handleSpotifyAuthButtonClick}>Authorize Spotify</button>
-          {showSpotifyAuth && <SpotifyAuthorizationComponent />}
-          </div>
+        <YouTubePlayer/>
       </div>
     </div>
   );
