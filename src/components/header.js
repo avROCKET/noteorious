567d6e342202b466d71d7ff1a7f894b20072b1b1
avROCKET <119268304+avROCKET@users.noreply.github.com>
2023-03-31 00:29:20 -0400

    Logo, Mobile Optimization

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import Clock from "./Clock.js";
+import logo from "../assets/logo.jpg";
 export default function Header (props) {
    const { signOut } = props;
    
+   
    return (
-      <nav className="navbar navbar-light bg-dark">
+      <nav className="navbar navbar-custom">
          <div className="container-fluid">
-            <a href="/homepage" className="navbar-brand text-light">NOTEorious</a> 
+            <a href="/homepage" className="navbar-brand text-light"><img style={{ height: 45, marginRight: 10}} src={logo}/>NOTEorious</a> 
             <Clock />
             <div className="d-flex">
                <button onClick={signOut} className="btn btn-outline-success text-light" type="submit">Log out</button> 
